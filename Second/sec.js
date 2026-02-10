6bebfd82577c8e66529e03b3ff2341252d86ee51
Aman Kumar Rai <aman.kumar.rai1203@gmail.com>
2026-02-10 05:00:27 +0000

    changes done

--- Second/sec.js
+++ Second/sec.js
@@ -1,1 +1,12 @@
-console.log("This is 2nd Day");
+// console.log("This is 2nd Day");
+
+// let a=10;
+// let b=20;
+
+// console.log(a);
+// console.log(b);
+
+let n=5;
+for (i = 1; i <= 10; i++) {
+    console.log(n + "*" + i + "=" + n*i);
+}
