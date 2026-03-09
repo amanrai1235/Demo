106711ee4f982954c737ed6200eacdbd6c44a5ed
Aman Kumar Rai <aman.kumar.rai1203@gmail.com>
2026-03-09 16:07:24 +0000

    done

--- day2/src/App.jsx
+++ day2/src/App.jsx
@@ -2,13 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// import Student from './Student.jsx'
 import Resume from './Resume.jsx'
 
 function App() {
   return (
     <>
-      {/* <Student></Student> */}
       <Resume></Resume>
     </>
   )
--- day2/src/Resume.jsx
+++ day2/src/Resume.jsx
@@ -1,11 +1,62 @@
-function Resume() {
-    return(
-        <div>
-            <h1>My Resume</h1>
-            <h2><b>Name :- </b>Aman Kumar Rai</h2>
-            <h2><b>Course :- </b>B.Tech</h2>
-            <h2><b>Clg :- </b>UCER</h2>
-        </div>
-    )
-}
-export default Resume
+import React from "react";
+
+const Resume = () => {
+  return (
+    <div style={{fontFamily:"Arial", padding:"40px", maxWidth:"900px", margin:"auto"}}>
+      
+      <h1>Aman Kumar Rai</h1>
+      <p>Buxar, Bihar | +91 9801368662</p>
+      <p>aman.kumar.rai1203@gmail.com</p>
+
+      <hr/>
+
+      <h2><u>Objective</u></h2>
+      <p>
+        Seeking an internship or entry-level role that offers hands-on experience,
+        learning opportunities, and a platform to convert theoretical knowledge
+        into practical solutions.
+      </p>
+      <br />
+
+      <h2><u>Education</u></h2>
+      <ul>
+        <li>
+          <b>B.Tech (CSE)</b> – United College of Engineering & Research, Prayagraj
+          (Expected 2027)
+        </li>
+        <li>
+          <b>Intermediate</b> – Springfield Public School, Ambala (2021–2023)
+        </li>
+        <li>
+          <b>High School</b> – Foundation School, Buxar (2011–2021)
+        </li>
+      </ul>
+      <br />
+
+      <h2><u>Technical Skills</u></h2>
+      <p><b>Web Development:</b> HTML, CSS, JavaScript</p>
+      <p><b>Programming Languages:</b> Java, C, Python</p>
+      <p><b>Core Concepts:</b> OOP, Basic Data Structures, DBMS Fundamentals</p>
+      <br />
+
+      <h2><u>Academic Projects</u></h2>
+      <ul>
+        <li>Developed basic programs using Java and Python.</li>
+        <li>Implemented logical solutions using structured and OOP approaches.</li>
+        <li>Worked with SQL queries for insert, update, delete and retrieve operations.</li>
+      </ul>
+      <br />
+
+      <h2><u>Strengths</u></h2>
+      <ul>
+        <li>Strong logical and analytical thinking</li>
+        <li>Quick learner with adaptability to new technologies</li>
+        <li>Good communication and teamwork skills</li>
+        <li>Dedicated and disciplined towards career growth</li>
+      </ul>
+
+    </div>
+  );
+};
+
+export default Resume;
